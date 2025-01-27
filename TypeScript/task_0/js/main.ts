5eea4f6fa5fea2a16f2c3cc642729c2553982376
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-27 10:40:16 +0100

    Add bases files

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,11 @@
+interface Student {
+    firstName: string;
+    lastName: number;
+    age: number;
+    location: string;
+  }
+
+  let student1: Student;
+  let student2: Student;
+  const studentsList: Student[] = [student1, student2];
+
